4814f2e33872228f9e0fc81e0d57365188fb4f66
lllypik <blesphoton@gmail.com>
2022-10-29 12:40:43 +0300

    Добавьте файлы проекта.

---
+++ Revit2022API_371/MainViewViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revit2022API_371
+{
+    class MainViewViewModel
+    {
+        private ExternalCommandData _commandData;
+
+        public MainViewViewModel(ExternalCommandData commandData)
+        {
+            _commandData = commandData;
+        }
+    }
+}
